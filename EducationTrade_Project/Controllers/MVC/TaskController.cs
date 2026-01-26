95fe36f2a6c6f60308a81f6601bba2b668052354
NoraizAsad@95 <noraizasad95@gmail.com>
2026-01-26 13:17:06 +0200

    Add TaskController in MVC

---
+++ EducationTrade_Project/Controllers/MVC/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using EducationTrade.Core.DTOs;
+using EducationTrade.Core.Interfaces;
+using EducationTrade.Presentation.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace EducationTrade.Presentation.Controllers.MVC
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskService _taskService;
+        private readonly IUserRepository _userRepository;
+        public TaskController(ITaskService taskService, IUserRepository userRepository)
+        {
+            _taskService = taskService;
+            _userRepository = userRepository;
+        }
+        public async Task<IActionResult> Browse()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if(userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            var user = await _userRepository.GetByIdAsync(userId.Value);
+            var result = await _taskService.GetAvailableTaskAsync();
+
+            var viewModel = new TaskViewModel()
+            {
+                currentUserId = userId.Value,
+                Tasks = result.Data,
+                UserCoinBalance = user.CoinBalance
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ EducationTrade_Project/ViewModel/TaskViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EducationTrade.Core.Entities;
+
+namespace EducationTrade.Presentation.ViewModel
+{
+    public class TaskViewModel
+    {
+        public int currentUserId { get; set; }
+        public List<Core.Entities.Task> Tasks { get; set; }
+        public int UserCoinBalance { get; set; }
+    }
+}
